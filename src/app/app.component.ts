c36dd99e13b4f3aa6af576467e06eb6bd2a24384
Ashvini Kumar <ashvinikumar45@gmail.com>
2017-06-09 09:50:49 +0530

    form data to array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
+
+  register(argument) {
+
+	var formObject = [
+		{
+			name: (<HTMLInputElement>document.getElementById('name')).value,
+			username: (<HTMLInputElement>document.getElementById('username')).value,
+			age: (<HTMLInputElement>document.getElementById('age')).value,
+			location: (<HTMLInputElement>document.getElementById('location')).value,
+			interests: (<HTMLInputElement>document.getElementById('interests')).value
+		}
+	]
+
+	console.log(formObject);
+}
+
 }
