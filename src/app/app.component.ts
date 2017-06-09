6b9043054d10a8b646fff9c436604deef12902b4
Ashvini Kumar <ashvinikumar45@gmail.com>
2017-06-09 11:32:54 +0530

    partial storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'app';
-
+  formObjects = [];
   register(argument) {
 
-	var formObject = [
+	var formObject = 
 		{
 			name: (<HTMLInputElement>document.getElementById('name')).value,
 			username: (<HTMLInputElement>document.getElementById('username')).value,
@@ -20,9 +20,16 @@ export class AppComponent {
 			location: (<HTMLInputElement>document.getElementById('location')).value,
 			interests: (<HTMLInputElement>document.getElementById('interests')).value
 		}
-	]
 
-	console.log(formObject);
+	this.formObjects.push(formObject)
+	localStorage['users'] = JSON.stringify({shit: this.formObjects})	
+
+	//var retrievedObject = localStorage['formObject'];
+
+	//console.log(retrievedObject);
+
+	console.log('retrievedObject: ', JSON.parse(localStorage['users']));
+
 }
 
 }
