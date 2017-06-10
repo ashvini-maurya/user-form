0b273dcf10a0e28b86b64782ba06beaaaccea04e
Ashvini Kumar <ashvinikumar45@gmail.com>
2017-06-10 21:30:35 +0530

    bootstraped form and table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  title = 'app';
-  
+	title = 'app';
 
-  register(argument) {
-  	var formObjects = []
-  	if ( "users" in localStorage)
-  		formObjects = JSON.parse(localStorage['users'])['root']
-  	else
-  		formObjects = []
+	register(argument) {
+		var formObjects = []
+		if ( "users" in localStorage)
+			formObjects = JSON.parse(localStorage['users'])['root']
+		else
+			formObjects = []
 
-  	console.log(formObjects);
-
-	var formObject = 
+		var formObject = 
 		{
 			name: (<HTMLInputElement>document.getElementById('name')).value,
 			username: (<HTMLInputElement>document.getElementById('username')).value,
@@ -28,19 +25,10 @@ export class UserComponent implements OnInit {
 			interests: (<HTMLInputElement>document.getElementById('interests')).value
 		}
 
-	formObjects.push(formObject)
-	localStorage['users'] = JSON.stringify({root: formObjects})	
+		formObjects.push(formObject)
+		localStorage['users'] = JSON.stringify({root: formObjects})	
 
-
-	//console.log('retrieved Object: ', JSON.parse(localStorage['users']));
-
-   // }
-
-
-}
-
-
-
+	}
 
 
 	constructor() {
@@ -48,6 +36,6 @@ export class UserComponent implements OnInit {
 	}
 
 	ngOnInit() {
-  	}
+	}
 
 }
