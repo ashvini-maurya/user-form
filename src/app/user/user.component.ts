31f50ec27b90ec13a00b5c1210b2619fbeaa7a3c
Ashvini Kumar <ashvinikumar45@gmail.com>
2017-06-11 21:26:10 +0530

    form validation except location

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,6 +9,69 @@ export class UserComponent implements OnInit {
 
 	title = 'app';
 
+
+	// inside object validation
+	present(key, value) {
+		if (value.length > 0) {
+			return true
+		} else {
+			return { error: true, message: key + " is not present!"}.message
+		}
+	}
+
+	unique(key, value) {
+		var parsedLocalStorageLength = JSON.parse(localStorage['users'])['root'].length
+		var localStorageUserNames = [];
+		for (var i = 0; i < parsedLocalStorageLength; i++) {
+			localStorageUserNames.push(JSON.parse(localStorage['users'])['root'][i]['username'])
+		}
+		if (localStorageUserNames.indexOf(value) !== -1) {
+			return { error: true, message: "username already taken, select another username" }.message
+		} else {
+			return true
+		}
+	}
+
+	nameValidation(name) {
+		return this.present("name", name)
+	}
+
+	usernameValidation(username) {
+		return this.unique("username", username)	
+	}
+
+	ageValidation(age) {
+		if (age >= 18) {
+			return true
+		} else {
+			return { error: true, message: "You should be atleast 18 years old!"}.message
+		}
+	}
+
+	locationValidation(location) {
+		return this.present("location", location)
+	}
+
+	interestsValidation(interests) {
+		if (interests.indexOf(',') > -1) {
+			return true
+		} else {
+			return { error: true, message: "Please insert atleast two interests with comma separated!"}.message
+		}
+	}
+
+	validation(object) {
+		var validatedName = this.nameValidation(object.name)
+		var validatedUserName = this.usernameValidation(object.username)
+		var validatedAge = this.ageValidation(object.age)
+		var validatedInterests = this.interestsValidation(object.interests)
+
+		if (validatedName != true || validatedUserName !== true || validatedAge != true || validatedInterests != true) {
+			alert("You have problem in your form: " + validatedName + " " + validatedUserName + " " + validatedAge + " " + validatedInterests)
+		}
+	};
+
+
 	register(argument) {
 		var formObjects = []
 		if ( "users" in localStorage)
@@ -22,16 +85,15 @@ export class UserComponent implements OnInit {
 			username: (<HTMLInputElement>document.getElementById('username')).value,
 			age: (<HTMLInputElement>document.getElementById('age')).value,
 			location: (<HTMLInputElement>document.getElementById('location')).value,
-			interests: (((<HTMLInputElement>document.getElementById('interests')).value).split(" ")).join(", ")
+			interests: (<HTMLInputElement>document.getElementById('interests')).value
 		}
 
-		// alert(formObject)
-
-		formObjects.push(formObject)
-		localStorage['users'] = JSON.stringify({root: formObjects})	
-
+		//form Object validation
+		if (this.validation(formObject)) {
+			formObjects.push(formObject)
+			localStorage['users'] = JSON.stringify({root: formObjects})
+		}		
 	}
-
 
 	constructor() {
 		// console.log(this.title);
